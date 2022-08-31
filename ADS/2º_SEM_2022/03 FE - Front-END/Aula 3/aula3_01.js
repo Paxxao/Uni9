faf7a152f170e5b81b2abdfc9828faf659f734fe
Gabriel Paixão Rodrigues da Silva <gabrielsovsk.dev@outlook.com>
2022-08-30 22:06:20 -0300

    Declaração de variavel e tipos de valores

---
+++ ADS/2º_SEM_2022/03 FE - Front-END/Aula 3/aula3_01.js
@@ -0,0 +1,11 @@
+let oct = 010;
+let bin = 0b10101, hexa = 0x10;
+let dia, nota, cep;
+dia = 25;
+nota = 9.7;
+cep = "0112223335"
+document.write (dia);
+document.write("<br>");
+document.write (nota);
+document.write("<br>");
+document.write(oct,"<br>",bin,"<br>",hexa,"<br>","CEP: ", cep);
