9a35ca96198601710a1e020e0d0405793b137356
Gabriel Paixão Rodrigues da Silva <110945620+Paxxao@users.noreply.github.com>
2023-11-22 22:02:25 -0300

    Add files via upload

---
+++ ADS/2º_SEM_2023/04 PV - Program. Visual/Urna/UrnaEletronica/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ProjetoUrnaEletronica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCandidato = new System.Windows.Forms.Label();
+            this.lblPartido = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnFinalizarVotacao = new System.Windows.Forms.Button();
+            this.btnConfirmarVoto = new System.Windows.Forms.Button();
+            this.txtCodigo = new System.Windows.Forms.MaskedTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCandidato
+            // 
+            this.lblCandidato.AutoSize = true;
+            this.lblCandidato.Location = new System.Drawing.Point(70, 38);
+            this.lblCandidato.Name = "lblCandidato";
+            this.lblCandidato.Size = new System.Drawing.Size(58, 13);
+            this.lblCandidato.TabIndex = 0;
+            this.lblCandidato.Text = "Candidato:";
+            // 
+            // lblPartido
+            // 
+            this.lblPartido.AutoSize = true;
+            this.lblPartido.Location = new System.Drawing.Point(360, 38);
+            this.lblPartido.Name = "lblPartido";
+            this.lblPartido.Size = new System.Drawing.Size(43, 13);
+            this.lblPartido.TabIndex = 1;
+            this.lblPartido.Text = "Partido:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(360, 191);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(70, 88);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(40, 13);
+            this.lblInfo.TabIndex = 3;
+            this.lblInfo.Text = "Status:";
+            // 
+            // btnFinalizarVotacao
+            // 
+            this.btnFinalizarVotacao.Location = new System.Drawing.Point(363, 165);
+            this.btnFinalizarVotacao.Name = "btnFinalizarVotacao";
+            this.btnFinalizarVotacao.Size = new System.Drawing.Size(107, 23);
+            this.btnFinalizarVotacao.TabIndex = 5;
+            this.btnFinalizarVotacao.Text = "Finalizar Votação";
+            this.btnFinalizarVotacao.UseVisualStyleBackColor = true;
+            this.btnFinalizarVotacao.Click += new System.EventHandler(this.btnFinalizarVotacao_Click_1);
+            // 
+            // btnConfirmarVoto
+            // 
+            this.btnConfirmarVoto.Location = new System.Drawing.Point(73, 169);
+            this.btnConfirmarVoto.Name = "btnConfirmarVoto";
+            this.btnConfirmarVoto.Size = new System.Drawing.Size(96, 23);
+            this.btnConfirmarVoto.TabIndex = 6;
+            this.btnConfirmarVoto.Text = "Confirmar Voto";
+            this.btnConfirmarVoto.UseVisualStyleBackColor = true;
+            this.btnConfirmarVoto.Click += new System.EventHandler(this.btnConfirmarVoto_Click);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(73, 143);
+            this.txtCodigo.Mask = "000";
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 7;
+            this.txtCodigo.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txtCodigo_MaskInputRejected);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(363, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 312);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.btnConfirmarVoto);
+            this.Controls.Add(this.btnFinalizarVotacao);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblPartido);
+            this.Controls.Add(this.lblCandidato);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCandidato;
+        private System.Windows.Forms.Label lblPartido;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnFinalizarVotacao;
+        private System.Windows.Forms.Button btnConfirmarVoto;
+        private System.Windows.Forms.MaskedTextBox txtCodigo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ADS/2º_SEM_2023/04 PV - Program. Visual/Urna/UrnaEletronica/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProjetoUrnaEletronica
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<int, string> candidatos = new Dictionary<int, string>
+    {
+        { 111, "Toddy - PepsiCo" },
+        { 222, "Necau - Nestlé" },
+        { 333, "Chocollato - 3corações" },
+        { 444, "Ovomaltine - ABF" }
+    };
+
+        private Dictionary<int, int> votos = new Dictionary<int, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Código a ser executado quando o formulário for carregado
+            lblCandidato.Text = "Candidato: N/A";
+            lblPartido.Text = "Partido: N/A";
+            lblInfo.Text = "Insira o código do candidato e pressione 'Confirmar Voto'.";
+            lblResultado.Text = "Resultado: ";
+            txtCodigo.Clear();
+
+            txtCodigo.TextChanged += test;
+
+            // Adicione aqui qualquer outra inicialização necessária.
+        }
+
+
+
+
+        // Evento acionado quando o usuário insere o código do candidato e pressiona Enter
+        private void txtCodigo_KeyDown(object sender, KeyEventArgs e)
+        {
+          
+            if (e.KeyCode == Keys.Enter)
+            {
+                ProcessarVoto();
+            }
+        }
+
+        // Método para processar o voto
+        private void ProcessarVoto()
+        {
+            if (int.TryParse(txtCodigo.Text, out int codigoCandidato))
+            {
+                if (candidatos.ContainsKey(codigoCandidato))
+                {
+                    if (votos.ContainsKey(codigoCandidato))
+                    {
+                        votos[codigoCandidato]++;
+                    }
+                    else
+                    {
+                        votos[codigoCandidato] = 1;
+                    }
+
+                    txtCodigo.Clear();
+                    lblInfo.Text = "Voto registrado com sucesso!";
+
+                    // Separar o nome do candidato e do partido
+                    string[] infoCandidato = candidatos[codigoCandidato].Split('-');
+
+                    // Atualizar as labels com as informações do candidato e do partido
+                    lblCandidato.Text = $"Candidato: {infoCandidato[0].Trim()}";
+                    lblPartido.Text = $"Partido: {infoCandidato[1].Trim()}";
+                    lblCandidato.Text = "Candidato:";
+                    lblPartido.Text = "Partido:";
+                }
+                else
+                {
+                    lblInfo.Text = "Código de candidato inválido!";
+                    // Limpar as labels para indicar que não há candidato selecionado
+                    lblCandidato.Text = "Candidato: N/A";
+                    lblPartido.Text = "Partido: N/A";
+                }
+            }
+            else
+            {
+                lblInfo.Text = "Por favor, insira um código válido!";
+                // Limpar as labels para indicar que não há candidato selecionado
+                lblCandidato.Text = "Candidato: N/A";
+                lblPartido.Text = "Partido: N/A";
+            }
+        }
+
+
+
+        // Método para mostrar o resultado da votação
+        private void MostrarResultado()
+        {
+            lblResultado.Text = "Resultado da votação:\n";
+
+            foreach (var voto in votos)
+            {
+                lblResultado.Text += $"{candidatos[voto.Key]}: {voto.Value} votos\n";
+            }
+        }
+
+        private void btnConfirmarVoto_Click(object sender, EventArgs e)
+        {
+            ProcessarVoto();
+        }
+
+        private void btnFinalizarVotacao_Click_1(object sender, EventArgs e)
+        {
+            MostrarResultado();
+            MessageBox.Show("Votação finalizada!");
+        }
+        private void txtCodigo_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+            MessageBox.Show("Entrada inválida. Por favor, insira apenas números.");
+        }
+
+        private void test (object sender, EventArgs e)
+        {
+                if (txtCodigo.Text == "111")
+                {
+                    lblCandidato.Text = "Nescau";
+                    lblPartido.Text = "PepsiCo";
+                pictureBox1.Image = Properties.Resources.nescau;
+
+                }
+                else if (txtCodigo.Text == "222")
+            {
+                lblCandidato.Text = "Toddy";
+                lblPartido.Text = "Nestlé";
+                pictureBox1.Image = Properties.Resources.toddy;
+            }
+            else if (txtCodigo.Text == "333")
+            {
+                lblCandidato.Text = "Chocollato";
+                lblPartido.Text = "3corações";
+                pictureBox1.Image = Properties.Resources.chocolatto;
+            }
+            else if (txtCodigo.Text == "444")
+            {
+                lblCandidato.Text = "Ovomaltine";
+                lblPartido.Text = "ABF";
+                pictureBox1.Image = Properties.Resources.ovomaltine;
+            }
+
+
+        }
+
+
+    }
+
+}
